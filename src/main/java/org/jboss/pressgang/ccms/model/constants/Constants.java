59ff3b63c7b5780b8a586076f3f6ccb7a41639c1
Matthew Casperson <mcaspers@redhat.com>
2013-11-13 08:51:25 +1000

    Working on minhash

--- src/main/java/org/jboss/pressgang/ccms/model/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/model/constants/Constants.java
@@ -7,6 +7,27 @@ public class Constants {
      */
     public static final int DEFAULT_BATCH_SIZE = 15;
 
+    /**
+     * The number of min hashes to generate a topic signature with.
+     */
+    public static final int NUM_MIN_HASHES = 200;
+
+    /**
+     * Given the number of min hashes above, this is the number of rows
+     * that will approximately match documents with a 60% similarity
+     * with the formula Math.pow(1/b, 1/r) (See http://infolab.stanford.edu/~ullman/mmds/ch3.pdf
+     * for more details).
+     */
+    public static final int LSH_SIXTY_PERCENT_ROWS = 7;
+
+    /**
+     * Given the number of min hashes above, this is the number of rows
+     * that will approximately match documents with a 90% similarity
+     * with the formula Math.pow(1/b, 1/r) (See http://infolab.stanford.edu/~ullman/mmds/ch3.pdf
+     * for more details).
+     */
+    public static final int LSH_NINETY_PERCENT_ROWS = 22;
+
     private Constants() {
     }
 }

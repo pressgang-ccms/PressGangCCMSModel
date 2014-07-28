43b8738923aeebcbd353bfb4bf0edbd5e7617ae0
Matthew Casperson <matthewcasperson@gmail.com>
2014-07-28 13:30:10 +1000

    Added license info for LGPL v3

--- src/main/java/org/jboss/pressgang/ccms/model/TagToTag.java
+++ src/main/java/org/jboss/pressgang/ccms/model/TagToTag.java
@@ -1,3 +1,22 @@
+/*
+  Copyright 2011-2014 Red Hat
+
+  This file is part of PresGang CCMS.
+
+  PresGang CCMS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  PresGang CCMS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PresGang CCMS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.jboss.pressgang.ccms.model;
 
 // Generated Aug 8, 2011 11:54:01 AM by Hibernate Tools 3.4.0.CR1
